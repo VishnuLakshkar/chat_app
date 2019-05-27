11582da68084a0500d59897f2a745bd67497509b
VishnuLakshkar <vishnulakshkar@gmail.com>
2019-05-27 11:24:03 +0530

    Complete without real time

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
 	def create
 		@conversation = Conversation.get(current_user.id, params[:user_id])
+		redirect_to conversation_messages_path(@conversation)
 	end
 
 	def index
